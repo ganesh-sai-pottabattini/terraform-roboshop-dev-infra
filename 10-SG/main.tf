3e51764fe39135766df1767a71ad7e90833fb893
Sai <Sai@gmail.com>
2026-06-27 12:16:04 +0530

    root SG module

---
+++ 10-SG/main.tf
@@ -0,0 +1,8 @@
+module "sg" {
+    count = length(var.sg_names)
+    source = "git::https://github.com/ganesh-sai-pottabattini/terraform-aws-security-group.git?ref=master"
+    project = var.project
+    environment = var.environment
+    sg_name = replace(var.sg_names[count.index], "_","-")
+    vpc_id = local.vpc_id
+}
---
+++ 10-SG/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.48.0"
+    }
+  }
+
+    backend "s3" {
+        bucket = "remote-state-trivikram"
+        key = "rbsp-dev-infra-sg.tfstate"
+        region = "us-east-1"
+        encrypt = true
+        use_lockfile = true
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
