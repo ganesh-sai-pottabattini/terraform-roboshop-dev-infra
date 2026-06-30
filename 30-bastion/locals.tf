d7bbb54920748cbe8e33d6f042b3426f839d9e37
Sai <Sai@gmail.com>
2026-06-30 23:03:25 +0530

    Bastion Server folder for roboshop infra

---
+++ 30-bastion/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  ami_id = data.aws_ami.joindevops.id
+  public_subnet_id = split(",", data.aws_ssm_parameter.public_subnet_ids.value)[0] # roboshop-dev-public-1a
+  bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+  common_name = "${var.project}-${var.environment}"
+  common_tags = {
+    Project = var.project
+    Environment = var.environment
+    Terraform =  "true"
+  }
+}
---
+++ 30-bastion/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "bastion" {
+  ami = local.ami_id
+  instance_type = t3.micro
+  vpc_security_group_ids = local.bastion_sg_id
+  subnet_id = local.public_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.bastion.name
+  user_data = templatefile("${path.module}/bastion.sh.tftpl", {
+    partition_number = 4
+    extend_size = 30
+  })
+
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${local.common_name}-bastion"
+    }
+  )
+}
