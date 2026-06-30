4d89f904c60bf64e0655cfb9b2e045b5aa461399
Sai <Sai@gmail.com>
2026-07-01 00:20:27 +0530

    Database Server creation + their components setup folder for roboshop Infra

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -2,14 +2,17 @@ dnf install ansible -y
 mkdir -p /var/log/roboshop
 chown -R ec2-user:ec2-user /var/log/roboshop
 chmod 755 /var/log/roboshop
-touch /var/log/roboshop/ansible.log 
+
+touch /var/log/roboshop/ansible.log
 chmod 755 /var/log/roboshop/ansible.log
 
 component=$1 #mongodb
 environment=$2 #dev
 
 cd /home/ec2-user
-git clone https://github.com/ganesh-sai-pottabattini/ansible-roboshop-v2-roles.git
+if [ ! -d ansible-roboshop-v2-roles ]; then
+    git clone https://github.com/ganesh-sai-pottabattini/ansible-roboshop-v2-roles.git
+fi
 cd ansible-roboshop-v2-roles
 git pull
 ansible-playbook -e component=$component -e env=$environment 1-component-configure.yaml
